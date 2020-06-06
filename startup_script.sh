7c0960fd245cecb4f4dd708c3363c8e3ed558826
Aswin <aswin1906@gmail.com>
2020-06-06 17:52:57 +0700

    Docker memory usage

--- startup_script.sh
+++ startup_script.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 alias lsof="lsof -i -P -n | grep LISTEN"
+alias opf="ssh -L localhost:9091:localhost:9091 aswin1906@orthus.nic.uoregon.edu"
+alias core="ulimit -a" #https://developer.ibm.com/node/2016/08/15/exploring-node-js-core-dumps-using-the-llnode-plugin-for-lldb/
+alias docMemory="sudo baobab /var/lib/docker"
